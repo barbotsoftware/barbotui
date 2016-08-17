747fe43aa57ed4045934f8697720e8f5ba0dacb7
nyadav810 <nyadav@uw.edu>
2016-08-16 18:59:41 -0700

    Home: button background color, rounded background edges

--- BarBot/iOS/HomeViewController.cs
+++ BarBot/iOS/HomeViewController.cs
@@ -7,6 +7,12 @@ namespace BarBot.iOS
 	public partial class HomeViewController : UIViewController
 	{
 
+		public UIColor BarBotBlue = new UIColor(
+			new nfloat(4.0/255.0), 
+			new nfloat(75.0/255.0), 
+			new nfloat(154.0/255.0),
+			new nfloat(1.0));
+		
 		partial void SignInButton_TouchUpInside(UIButton sender)
 		{
 			Console.WriteLine("sign in segue");
@@ -29,6 +35,17 @@ namespace BarBot.iOS
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib
+			ConfigureView();
+		}
+
+		public void ConfigureView()
+		{
+			SignInButton.Layer.CornerRadius = RegisterButton.Layer.CornerRadius = new nfloat(5.0);
+			SignInButton.Layer.BorderWidth = RegisterButton.Layer.BorderWidth = new nfloat(0.9);
+			SignInButton.Layer.BorderColor = RegisterButton.Layer.BorderColor = BarBotBlue.CGColor;
+			var insets = new UIEdgeInsets(new nfloat(2.0), new nfloat(2.0), new nfloat(2.0),
+												   new nfloat(2.0));
+			SignInButton.TitleEdgeInsets = RegisterButton.TitleEdgeInsets = insets;
 		}
 
 		public override void DidReceiveMemoryWarning()
