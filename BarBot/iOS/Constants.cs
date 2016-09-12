9de135f59b6194d781b117772c5498a54e00093d
nyadav810 <nyadav@uw.edu>
2016-09-11 21:39:53 -0700

    added JSON.Net to shared project, renamed StyleUtil to Constants, started creating ViewModel objects

--- BarBot/iOS/Constants.cs
+++ BarBot/iOS/Constants.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using UIKit;
+
 namespace BarBot.iOS
 {
-	public class StyleUtil
+	struct Color
 	{
-		public UIColor BarBotBlue = new UIColor(
+		public static UIColor BarBotBlue = new UIColor(
 			new nfloat(4.0 / 255.0),
 			new nfloat(75.0 / 255.0),
 			new nfloat(154.0 / 255.0),
--- BarBot/iOS/ViewControllers/RegisterViewController.cs
+++ BarBot/iOS/ViewControllers/RegisterViewController.cs
@@ -7,6 +7,7 @@ namespace BarBot.iOS
 	public class RegisterViewController : BaseUserValidationViewController
 	{
 		UISwitch privacySwitch;
+		UILabel privacyLabel;
 
 		public override void ViewDidLoad()
 		{
@@ -14,16 +15,33 @@ namespace BarBot.iOS
 			Title = "Register";
 			configureSubmitButton("Create Account");
 			configurePrivacySwitch();
+			configurePrivacyLabel();
 
-			View.AddSubviews(new UIView[] { submitButton, privacySwitch });
+			View.AddSubviews(new UIView[] { submitButton, privacySwitch, privacyLabel });
 		}
 
 		void configurePrivacySwitch()
 		{
 			privacySwitch = new UISwitch
 			{
-				Frame = new CGRect(View.Bounds.Width - 70, 260, 0, 0)
+				Frame = new CGRect(View.Bounds.Width - 70, 260, 0, 0),
+				//Action = privacySwitchChanged()
 			};
+		}
+
+		void configurePrivacyLabel()
+		{
+			privacyLabel = new UILabel
+			{
+				Frame = new CGRect(22, 268, View.Bounds.Width - 100, 15),
+				Text = "I agree to the Privacy Policy and Terms of Use",
+				Font = UIFont.SystemFontOfSize(13, UIFontWeight.Thin)
+			};
+		}
+
+		void privacySwitchChanged()
+		{
+			
 		}
 	}
 }
