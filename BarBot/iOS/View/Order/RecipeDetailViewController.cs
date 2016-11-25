4645168450a0010435f936de2ead6ba5d6c9facf
Naveen Yadav <nyadav@uw.edu>
2016-11-24 21:14:08 -0800

    MVVM file structure, added ReactiveUI packages

---
+++ BarBot/iOS/View/Order/RecipeDetailViewController.cs
@@ -0,0 +1,24 @@
+﻿using UIKit;
+using BarBot.iOS.ViewModel;
+
+namespace BarBot.iOS.Order
+{
+	public class RecipeDetailViewController : UIViewController
+	{
+		RecipeViewModel ViewModel;
+		UIImageView ImageView;
+
+		public RecipeDetailViewController(RecipeViewModel viewModel)
+		{
+			ViewModel = viewModel;
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			Title = ViewModel.RecipeName;
+			ImageView = new UIImageView(ViewModel.DrinkImage);
+			ImageView.ContentMode = UIViewContentMode.ScaleAspectFit;
+		}
+	}
+}
