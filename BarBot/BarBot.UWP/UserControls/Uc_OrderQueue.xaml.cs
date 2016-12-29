451e7f9e66d9eb6982ade6a3a04ae124528d4a2a
Alex <alexhors@uw.edu>
2016-12-29 14:20:16 -0800

    Updated order queue layout

--- BarBot/BarBot.UWP/UserControls/Uc_OrderQueue.xaml.cs
+++ BarBot/BarBot.UWP/UserControls/Uc_OrderQueue.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using BarBot.Core.Model;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -47,13 +48,33 @@ namespace BarBot.UWP.UserControls
             });
         }
 
-        private void PourButton_Click(object sender, RoutedEventArgs e)
+        private async void PourButton_Click(object sender, RoutedEventArgs e)
         {
             Recipe recipe = (sender as Button).Tag as Recipe;
 
             Dictionary<IO.Devices.IContainer, double> ingredients =  Utils.Helpers.GetContainersFromRecipe(recipe, app.barbotIOController.Containers);
 
             //app.barbotIOController.PourDrink(ingredients);
+
+            var dialog = new ContentDialog()
+            {
+                MaxWidth = this.ActualWidth,
+                Content = new TextBlock()
+                {
+                    Text = string.Format("Your {0} Is Pouring!", recipe.Name),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    FontSize = 45
+                },
+                Background = new SolidColorBrush(Windows.UI.Color.FromArgb(100, 19, 19, 19)),
+                Foreground = new SolidColorBrush(Windows.UI.Colors.White),
+                BorderBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(100, 34, 34, 34))
+            };
+
+            dialog.ShowAsync();
+
+            await Task.Delay(5000);
+
+            dialog.Hide();
         }
     }
 }
