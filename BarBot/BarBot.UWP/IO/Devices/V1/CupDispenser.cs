3bde810e65854f6bcd07b0b865acc049eadd2643
Alex <alexhors@uw.edu>
2017-12-19 23:22:46 -0800

    Added logic to read from MCP3008 analog to digital converter for cup dispenser

--- BarBot/BarBot.UWP/IO/Devices/V1/CupDispenser.cs
+++ BarBot/BarBot.UWP/IO/Devices/V1/CupDispenser.cs
@@ -13,23 +13,30 @@ namespace BarBot.UWP.IO.Devices.V1
     /// </summary>
     public class CupDispenser : ICupDispenser
     {
+        private int EMPTY_CUP_THRESHOLD = 100;
+        private int FULL_CUP_THRESHOLD = 500;
+
         public L298NDriver stepperDriver;
 
         public IOPort fsr1;
 
+        public MCP3008 mcp3008;
+
         public CupDispenser() { }
 
+        //TODO: Don't use hard coded channel number for analog to digital converted
         public CupDispenser(IIOPort stepper1, IIOPort stepper2, IIOPort stepper3, IIOPort stepper4, IOPort fsr1)
         {
             stepperDriver = new L298NDriver(stepper1, stepper2, stepper3, stepper4, 7);
             this.fsr1 = fsr1;
+            mcp3008.connect();
         }
 
         public void DispenseCup()
         {
             Debug.WriteLine(string.Format("Running cup dispenser"));
 
-            bool triggered = fsr1.GpioPin.Read().Equals(GpioPinValue.High);
+            bool triggered = mcp3008.read(0) >= EMPTY_CUP_THRESHOLD; // TODO: hard coded channel #
             if (!triggered)
             {
                 // Attempt to release a cup
@@ -41,7 +48,7 @@ namespace BarBot.UWP.IO.Devices.V1
                     Task.Delay(100);
 
                     // Check if the weight sensor has been triggered
-                    triggered = fsr1.GpioPin.Read().Equals(GpioPinValue.High);
+                    triggered = mcp3008.read(0) >= FULL_CUP_THRESHOLD; // TODO hard coded channel #
                 }
             }
         }
---
+++ BarBot/BarBot.UWP/IO/MCP3008.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Spi;
+using System.Diagnostics;
+
+namespace BarBot.UWP.IO
+{
+    /// <summary>
+    /// Analog to digital converter
+    /// </summary>
+    public class MCP3008
+    {
+        private const int CLOCK_FREQUENCY = 3600000; // 3.6 MHz
+
+        private SpiDevice _mcp3008;
+
+        public async void connect(int spiBus = 0)
+        {
+            var spiSettings = new SpiConnectionSettings(spiBus);
+            spiSettings.ClockFrequency = CLOCK_FREQUENCY;
+            spiSettings.Mode = SpiMode.Mode0;
+
+            string spiQuery = SpiDevice.GetDeviceSelector("SPI0");
+            var deviceInfo = await DeviceInformation.FindAllAsync(spiQuery);
+            if (deviceInfo != null && deviceInfo.Count > 0)
+            {
+                _mcp3008 = await SpiDevice.FromIdAsync(deviceInfo[0].Id, spiSettings);
+            }
+            else
+            {
+                Debug.WriteLine("MCP3008 device not found...");
+            }
+        }
+
+        /// <summary>
+        /// Read the value from the specified channel on the MCP3008. Must be 0 - 7.
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <returns></returns>
+        public int read(int channel)
+        {
+            //From data sheet -- 1 byte selector for channel 0 on the ADC
+            //First Byte sends the Start bit for SPI
+            //Second Byte is the Configuration Bit
+            //1 - single ended 
+            //0 - d2
+            //0 - d1
+            //0 - d0
+            //             S321XXXX <-- single-ended channel selection configure bits
+            // Channel 0 = 10000000 = 0x80 OR (8+channel) << 4
+            int channelByte = (8 + channel) << 4;
+            var transmitBuffer = new byte[3] { 1, (byte)channelByte, 0x00 };
+            var receiveBuffer = new byte[3];
+
+            _mcp3008.TransferFullDuplex(transmitBuffer, receiveBuffer);
+
+            //first byte returned is 0 (00000000), 
+            //second byte returned we are only interested in the last 2 bits 00000011 (mask of &3) 
+            //then shift result 8 bits to make room for the data from the 3rd byte (makes 10 bits total)
+            //third byte, need all bits, simply add it to the above result 
+            var result = ((receiveBuffer[1] & 3) << 8) + receiveBuffer[2];
+
+            Debug.WriteLine("Read value from MCP3008: " + result);
+
+            return result;
+        }
+    }
+}
